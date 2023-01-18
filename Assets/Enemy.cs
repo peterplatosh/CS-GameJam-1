2e1317902e5bb4b3da56e3f44f559a3f1f7fef19
PenguinMan <jbrauser17@gmail.com>
2023-01-18 12:25:39 -0800

    Added enemy movement

---
+++ Assets/Enemy.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform goal;
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, goal.position, moveSpeed * Time.deltaTime);
+
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Goal"))
+        {
+            print("You died!");
+        }
+    }
+}
